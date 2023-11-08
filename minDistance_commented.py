83dad8794721641eb530c8fc79c35a80cd2b8632
Florian <136637769+f-sure@users.noreply.github.com>
2023-11-08 10:02:10 +0100

    Update minDistance_commented.py
    Corrected intendations.

--- minDistance_commented.py
+++ minDistance_commented.py
@@ -46,53 +46,51 @@ with open((resultspath+".csv"), "w") as file:
 #Iterate through all residues in betaENaC 
 for betaRes in range(start_chain_1, stop_chain_1):
 
-    #Define the first interaction partner (the current residue in beta ENaC) 
-    com = analysis.Com(msys_model, cms_model, asl='(chain.name '+chain_name_1+') AND (res.num '+str(betaRes)+')')
+	#Define the first interaction partner (the current residue in beta ENaC) 
+	com = analysis.Com(msys_model, cms_model, asl='(chain.name '+chain_name_1+') AND (res.num '+str(betaRes)+')')
 
-    #Define the second interaction partner (the first residue in gamma ENaC)
+	#Define the second interaction partner (the first residue in gamma ENaC)
 	com2 = analysis.Com(msys_model, cms_model, asl='(chain.name '+chain_name_2+') AND (res.num '+str(start_chain_2)+')')
     
-    #Initialize a Distance Analyzer object from Schrödinger Desmond with the current MD simulation and the two interaction partners just defined
+	#Initialize a Distance Analyzer object from Schrödinger Desmond with the current MD simulation and the two interaction partners just defined
 	distanceAnalyzer = analysis.Distance(msys_model, cms_model, com, com2)
     
-    #Perform the analysis
+ 	#Perform the analysis
 	results = analysis.analyze(tr, distanceAnalyzer)
     
-    #The results variable now contains the first analysis for the current residue in betaENaC 
-    #It is a list with the distance from this residue to the first residue, each element in the list corresponds to the distance in the corresponding frame of the simulation
+	#The results variable now contains the first analysis for the current residue in betaENaC 
+	#It is a list with the distance from this residue to the first residue, each element in the list corresponds to the distance in the corresponding frame of the simulation
 
 	#Now, we loop through every other residue from the second interaction partner (gamma ENaC)
 	for gammaRes in range(start_chain_2 + 1, stop_chain_2):
 		try:
-            #Similar process as above - definition of second interaction partner 
+            		#Similar process as above - definition of second interaction partner 
 			com2 = analysis.Com(msys_model, cms_model, asl='(chain.name '+chain_name_2+') AND (res.num '+str(gammaRes)+')')
             
-            #Initialization of Distance Analyzer Object
+            		#Initialization of Distance Analyzer Object
 			distanceAnalyzer = analysis.Distance(msys_model, cms_model, com, com2)
             
-            #Perform the Analysis - save results in temp_results
+            		#Perform the Analysis - save results in temp_results
 			temp_results = analysis.analyze(tr, distanceAnalyzer)
             
-            #The temp_results variable now contains the distance of the current residue in gammaENaC in each frame 
-            #We iterate through our results 
+			#The temp_results variable now contains the distance of the current residue in gammaENaC in each frame 
+			
+			#We iterate through our results 
 			for i, res in enumerate(results):
             
-                #Now we compare if the distance of the current evaluation is lower than all previously identified comparisons 
+                		#Now we compare if the distance of the current evaluation is lower than all previously identified comparisons 
 				if(res > temp_results[i]):
                     
-                    #If this is the case, the lower distance is saved as the new "lowest distance" in the results array 
+                    			#If this is the case, the lower distance is saved as the new "lowest distance" in the results array 
 					results[i] = temp_results[i]
         
-		except: 
+ 		except: 
 			#This exception is reached when a residue to be analysed is not present in the structure - this is logged in the console
-            print("- EXCEPTION: "+str(gammaRes))
+			print("- EXCEPTION: "+str(gammaRes))
             
 	#After the iteration through all residues in gamma, the results variable contains the lowest distance for the current residue from interaction partner 1 (betaENaC) to all residues in interaction partner 2 (gammaENaC). 
     
-    #These results are appended to the results file 
+     	#These results are appended to the results file 
 	with open((resultspath+".csv"), "a") as file:
 		for i, res in enumerate(results):
 			file.write(str(i) + "," + str(betaRes) + "," + str(round(res, 2)) + "\n")
-			
-
-
